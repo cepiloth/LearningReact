9ea95cbb0ddc0cc63c7ffdb56ebfc8aaf0dabf74
demian <lim10260@nate.com>
2021-10-04 16:39:01 +0900

    react 실습

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      // 부모가 있어야한다. h1, h2 두개의 태그를 사용하려면 div tag 로 감싸줘야한다.
+      // Virtual DOM 이라고 한다.
+      <div>
+        <h1>리액트 안녕!</h1>
+        <h2>당신은 어썸한가요?</h2>
+      </div>
+    );
+  }
 }
 
 export default App;

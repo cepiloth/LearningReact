7f01a0b2baf885e9420de399ec632c9e866ab9c9
demian <lim10260@nate.com>
2021-10-04 17:23:00 +0900

    리액트학습
    컴포넌트 추가해서 App.js 에서 import  해서 사용하기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,34 +1,11 @@
 import React, {Component} from 'react';
-import './App.css'
+import MyComponet from './MyComponent'; // MyComponent 를 불러옵니다.
 
 class App extends Component {
   render() {
-    const text = '당신은 어썸한가요?';
-    const condition = true;
-    const style = {
-      backgroundColor: 'gray',
-      border: '1px solid black',
-      height: Math.round(Math.random() * 300) + 50,
-      width: Math.round(Math.random() * 300) + 50,
-      WekitTransition: 'all',
-      MozTransition: 'all',
-      msTransition: 'all'
-    }
+
     return (
-      // 부모가 있어야한다. h1, h2 두개의 태그를 사용하려면 div tag 로 감싸줘야한다.
-      // Virtual DOM 이라고 한다.
-      <div className="my-div">
-        <h1>리액트 안녕!</h1>
-        <h2>{text}</h2>
-        {
-          condition && '보여주세요'
-        }
-        <div 
-          style={style}>
-        </div>
-        // 여기 쓰는 건 그대로 렌더링됩니다.
-        /* 여기에선 주석 못 써요 */
-      </div>
+      <MyComponet></MyComponet>
     );
   }
 }

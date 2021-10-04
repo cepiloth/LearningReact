16a746146bf469e164b51891c47f460026d9feb3
demian <lim10260@nate.com>
2021-10-04 17:39:21 +0900

    리액트 state 를 이용하여 변경하기

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -11,11 +11,25 @@ class MyComponent extends Component {
         age: PropTypes.number.isRequired // 필수적으로 존재해야 하며, 숫자 입니다.
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0
+        }
+    }
+
     render() {
         return (
             <div>
                 <p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p>
                 <p>저는 {this.props.age}살 입니다.</p>
+                <p>숫자: {this.state.number}</p>
+                <button onClick={() => {
+                    this.setState({
+                        number: this.state.number + 1
+                    })
+                }
+                }>더하기</button>
             </div>
         )
     }

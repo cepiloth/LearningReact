00a91ea3a1896587a244480fc704f8a6eec5ed7f
cepiloth <lim10260@nate.com>
2021-10-17 18:16:36 +0900

    Update CMS
    https://www.youtube.com/watch?v=UT9U9WALGS0&list=PLRx0vPvlEmdD1pSqKZiTihy5rplxecNpz&index=5

--- crm/src/App.js
+++ crm/src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customer = [
+{
+  'id': 1,
+  'image' : 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202110/05/5c1cef2f-2b0e-4901-b52e-583170963bd0.jpg',
+  'name' : '홍길동',
+  'birthday' : '989861',
+  'gender' : '남자',
+  'job' : ' 대학생'
+},
+{
+  'id': 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍광표',
+  'birthday' : '283901',
+  'gender' : '남자',
+  'job' : ' 대학생'
+},
+{
+  'id': 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '이제동',
+  'birthday' : '289208',
+  'gender' : '남자',
+  'job' : ' 대학생'
+}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    {
+    }
   );
 }
 
